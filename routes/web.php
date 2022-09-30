70630a9dce033d3723b4f0af22f7a324cd1189e5
Alvin Valencia <raverazor@gmail.com>
2022-09-30 14:56:00 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('profile');
+});
+
+Route::get('/', function () {
+    return view('certificate');
+});
+
+Route::get('/', function () {
+    return view('education');
+});
+
+Route::get('/', function () {
+    return view('profile');
+});
+
+Route::get('/', function () {
+    return view('profile');
 });
